4a9f808d663111286767f83a59d7c1c2604c505b
TakuroOnoda <takuro.onoda@gmail.com>
2017-10-24 14:52:25 +0900

    add semantic-ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import './Home.css';
+import { Container } from 'semantic-ui-react'
 
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Karakuri Farm</h2>
+        <div className="Home">
+          <Container className="Home-header" style={{height: '240px' }}>
+            <img src={logo} className="Home-logo" alt="logo" />
+            <h2>Welcome to Karakuri Farm</h2>
+          </Container>
+          <p className="Home-intro">
+            Karakuri Farm is the best farm automation system.
+          </p>
         </div>
-        <p className="Home-intro">
-          Karakuri Farm is the best farm automation system.
-        </p>
-      </div>
     );
   }
 }
