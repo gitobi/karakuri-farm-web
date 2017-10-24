1ab89c37fd46d8d814b915b8b420632f91537f3b
TakuroOnoda <takuro.onoda@gmail.com>
2017-10-24 14:57:02 +0900

    minor fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,15 @@ import { Container } from 'semantic-ui-react'
 class Home extends Component {
   render() {
     return (
-        <div className="Home">
-          <Container className="Home-header" style={{height: '240px' }}>
-            <img src={logo} className="Home-logo" alt="logo" />
-            <h2>Welcome to Karakuri Farm</h2>
-          </Container>
-          <p className="Home-intro">
-            Karakuri Farm is the best farm automation system.
-          </p>
-        </div>
+      <div className="Home">
+        <Container className="Home-header" style={{height: '240px' }}>
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h2>Welcome to Karakuri Farm</h2>
+        </Container>
+        <p className="Home-intro">
+          Karakuri Farm is the best farm automation system.
+        </p>
+      </div>
     );
   }
 }
