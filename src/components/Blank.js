6c8622f3dc31c227c0a8b8cd351f4fe5daaf2b8a
TakuroOnoda <takuro.onoda@gmail.com>
2017-10-24 15:46:56 +0900

    apply tow column layout

---
+++ src/components/Blank.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Blank extends Component {
+
+  render() {
+    return (
+      <div>
+        blank page
+      </div>
+    );
+  }
+}
