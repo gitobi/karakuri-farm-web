550cf8585ecb1a7ebe615c11048bf02f001c9a7a
TakuroOnoda <takuro.onoda@gmail.com>
2017-11-13 03:42:39 +0900

    OperationalRecords取得APIの実装

--- src/containers/DevicesWateringOperationalRecords.js
+++ src/containers/DevicesWateringOperationalRecords.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import Logger from '../js/Logger'
+import Bastet from '../js/Bastet'
 
 class DevicesWateringOperationalRecords extends React.Component {
   constructor(props) {
     super(props);
     this.logger = new Logger({prefix: 'DevicesWateringOperationalRecords'});
+
+    this.testCallApi = this.testCallApi.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps, nextState) {
+    if (this.props.deviceId !== nextProps.deviceId) {
+      // deviceId変更時
+      this.logger.info('componentWillReceiveProps', "nextProps", nextProps);
+      this.testCallApi(nextProps.deviceId);
+    }
+  }
+
+  testCallApi(deviceId) {
+    var bastet = new Bastet()
+    bastet.getWateringsOperationalRecords(deviceId).then(
+      result => this.logger.log('result', result)
+      ,error => this.logger.log('error', error)
+    );
   }
 
   render() {
-
     return (
         <div className="ui container">
           <div className="item ui header">
