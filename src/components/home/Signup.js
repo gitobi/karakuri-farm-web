d1e10c8ada5a6b26b166bc64579ce2eb0609e95d
Rui Onodera <deraru@gmail.com>
2017-11-18 23:32:00 +0900

    Implement Signup page

--- src/components/home/Signup.js
+++ src/components/home/Signup.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  Segment,
+} from 'semantic-ui-react';
 import HomeLayout from '../../layouts/HomeLayout';
+import SignupForm from '../../containers/home/SignupForm';
 
 class Signup extends Component {
   render() {
     return (
       <HomeLayout>
-        <h1>Signup Page</h1>
+        <Segment
+          vertical >
+          <Container>
+            <h1>会員登録</h1>
+            <SignupForm />
+          </Container>
+        </Segment>
       </HomeLayout>
     );
   }
