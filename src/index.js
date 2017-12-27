589b0e57c833d14f145b1565842bb8ff1eb2c257
TakuroOnoda <takuro.onoda@gmail.com>
2017-12-28 00:51:44 +0900

    add redux-devtool

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import { render } from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import karakuriFarmApp from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import AppRoute from './containers/AppRoute'
 
+const composeEnhancers = composeWithDevTools({
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
 const store = createStore(
   karakuriFarmApp,
-  applyMiddleware(
-    thunkMiddleware
+  composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware
+    )
   )
 );
 
