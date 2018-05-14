8882e03bb09b27e963fc19039abae20b25adc671
TakuroOnoda <takuro.onoda@gmail.com>
2018-05-14 17:34:50 +0900

    device一覧にtableを適用

--- src/containers/device/DevicesIndex.js
+++ src/containers/device/DevicesIndex.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import {Container} from 'semantic-ui-react';
+import EditableTable from '../../components/common/EditableTable'
 import Logger from '../../js/Logger'
-
-import DevicesIndexItem from '../../components/device/DevicesIndexItem'
 
 import { connect } from 'react-redux';
 
@@ -15,25 +15,46 @@ class DevicesIndex extends Component {
   render() {
     // this.logger.log(this.props)
 
-    const indexItems = [];
-    for (var i in this.props.devices) {
-      let item = this.props.devices[i];
-      indexItems.push(
-          <DevicesIndexItem key={item.id} item={item}/>
-        );
-    }
+    // テーブルのカラムレイアウト
+    const columns = [{
+        Header: 'ID',
+        accessor: 'id',
+        width: 360,
+        customCell: { type: 'normal', divStyle: {textAlign: 'left'}},
+      }, {
+        Header: 'Name',
+        accessor: 'name',
+        width: 320,
+        customCell: { type: 'normal', divStyle: {textAlign: 'left'}},
+      }, {
+        Header: 'Heartbeated at',
+        accessor: 'heartbeated_at',
+        width: 180,
+        customCell: { type: 'normal', divStyle: {textAlign: 'left'}},
+    }];
 
     return (
-      <div>
-        {indexItems}
-      </div>
+      <Container>
+        <EditableTable
+          label={'Devices'}
+          data={this.props.devices}
+          columns={columns}
+          loading={this.props.progress}
+          filterable={true}
+          sortable={true}
+          defaultPageSize={50}
+          defaultSorted={[{id: 'name', desc: true}]}
+        />
+
+      </Container>
     );
   }
 }
 
 function mapStateToProps(state) {
   return  {
-    devices: state.device.getIn(['devices']).toJS(),
+    devices:  state.device.getIn(['devices']).toJS(),
+    progress: state.device.get('progress'),
   };
 }
 
