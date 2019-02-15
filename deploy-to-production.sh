1d84b52534f21a360c5541071880823607dbb2f7
TakuroOnoda <takuro.onoda@gmail.com>
2019-02-15 12:38:33 +0900

    fix deploy job

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 cp .env.production-env .env.production
+cp .env.production-env.local .env.production.local
 yarn build
 aws s3 cp build s3://karakuri.farm --recursive --exclude .DS_Store
 aws cloudfront create-invalidation --distribution-id E3QY46YGHFXMOK --paths /\*
