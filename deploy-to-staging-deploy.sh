15067bc7e16609b14d6e8b384b5a741074b89b50
TakuroOnoda <takuro.onoda@gmail.com>
2019-02-16 16:13:00 +0900

    fix deploy job

--- deploy-to-staging-deploy.sh
+++ deploy-to-staging-deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
+ls -a
+pwd
+aws --version
+type aws
 aws s3 cp build s3://staging.karakuri.farm --recursive --exclude .DS_Store
 aws cloudfront create-invalidation --distribution-id EW2H0JKSXZQ6B --paths /\*
