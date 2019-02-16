9945ce50c1987579d043f234dbe2a04120db5151
TakuroOnoda <takuro.onoda@gmail.com>
2019-02-16 16:06:23 +0900

    fix deploy job

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
-cp .env.production-env .env.production
-cp .env.production-env.local .env.production.local
-yarn build
-aws s3 cp build s3://karakuri.farm --recursive --exclude .DS_Store
-aws cloudfront create-invalidation --distribution-id E3QY46YGHFXMOK --paths /\*
+sh ./deploy-to-production-build.sh
+sh ./deploy-to-production-deploy.sh
--- deploy-to-staging.sh
+++ deploy-to-staging.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
-cp .env.staging-env .env.production
-cp .env.staging-env.local .env.production.local
-yarn build
-aws s3 cp build s3://staging.karakuri.farm --recursive --exclude .DS_Store
-aws cloudfront create-invalidation --distribution-id EW2H0JKSXZQ6B --paths /\*
+sh ./deploy-to-staging-build.sh
+sh ./deploy-to-staging-deploy.sh
